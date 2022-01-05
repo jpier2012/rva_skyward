23966d90cf61d35d89576d8eda73b90a523b77de
James Pier <jpier2012@gmail.com>
2022-01-05 16:41:32 -0600

    converted reusable code into helper methods

--- RVA Skyward Script.java
+++ RVA Skyward Script.java
@@ -1,85 +1,105 @@
-String uName = '{!$Credential.UserName}';
-String pWord = '{!$Credential.Password}';
-String reqBody = 'grant_type=password&username=' + uName + '&password=' + pWord;
-String endpoint = 'callout:RVA_Skyward';
-String tokenEndpoint = endpoint + '/token';
-    
-String authToken;
-String studentsEndpoint = endpoint + '/v1/students';
-String schoolsEndpoint = endpoint + '/v1/schools';
-    
-// Request token from the token endpoint
+// username and password from the named credential
+private String endpoint = 'callout:RVA_Skyward';
+private String uName = '{!$Credential.UserName}';
+private String pWord = '{!$Credential.Password}';
+private String AUTH_TOKEN = getAuthToken();
 
-// send the HTTP request to the token endpoint with the username and password credentials 
-// in the BODY of the request
-HttpRequest req = new HttpRequest();
-req.setMethod('POST');
-req.setEndpoint(tokenEndpoint);
-req.setBody(reqBody);
-req.setHeader('Content-Type', 'application/x-www-form-urlencoded');
-req.setHeader('Content-Length', '\'reqBody.length()\'');
+private String SCHOOL_ID = '800';
 
-// Parse the response into JSON
-HttpResponse res = new Http().send(req);
-valence.JSONParse root = new valence.JSONParse(res.getBody());
-
-// assign the auth token to authToken
-authToken = root.get('access_token').getStringValue();
+// private String studentsEndpoint = endpoint + '/v1/students';
+private String schoolsEndpoint = endpoint + '/v1/schools';
 
 // GET sample data from the API using the token
 HttpRequest schoolsReq = new HttpRequest();
 schoolsReq.setMethod('GET');
 schoolsReq.setEndpoint(schoolsEndpoint);
-schoolsReq.setHeader('Authorization', 'Bearer ' + authToken);
+schoolsReq.setHeader('Authorization', 'Bearer ' + AUTH_TOKEN);
 schoolsReq.setHeader('Content-Type', 'application/json');
 
-// Take the disctrict JSON object and pull out values
+// Parse the returned body as a list of objects
 HttpResponse schoolsRes = new Http().send(schoolsReq);
 List<valence.JSONParse> schoolDataRoot = new valence.JSONParse(schoolsRes.getBody()).asList();
-Map<String, valence.JSONParse> firstRecord = new valence.JSONParse(schoolsRes.getBody()).get('[0]').asMap();
-List<String> fields = new List<String>(firstRecord.keySet());
-
-String header = '';
-
-for (String item : fields ) {
-    header = header + item + ',';
-}
-
-header = header + '\n';
-
-// System.debug('Object? : ' + testRoot.isObject());
-// System.debug('Array? : ' + testRoot.isArray());
-
-String csvName = 'RVA Schools.csv';
-String subject = 'RVA Schools data from the API';
-String recordString = '';
-String finalString = '';
-
-for (valence.JSONParse school : schoolDataRoot) {
-    for (String field : fields) {
-        recordString = recordString + school.get(field).getStringValue() + ',';
-    }
-    finalString = finalString + recordString + '\n';
-    recordString = '';
-}
-
-finalString = header + finalString;
-    
+List<String> fields = new List<String>(schoolDataRoot[0].asMap().keySet());
+   
 // send an email with the .csv file
-
+String csvName = 'RVA Schools.csv';
+String dataString = createCsvString(fields, schoolDataRoot);
+String subject = 'RVA Schools data from the API';
 List<String> toAddresses = new List<String> {'james.pier@servioconsulting.com'};
+    
+sendCsvEmail(csvName, dataString, subject, toAddresses);
 
-Blob csvBlob = Blob.valueOf(finalString);
+/////////////////////////////////////////////////
+//////////////////  Helpers  ////////////////////
+/////////////////////////////////////////////////
 
-Messaging.EmailFileAttachment csvAttachment = new Messaging.EmailFileAttachment();
+// Return the token from the token endpoint
+private String getAuthToken() {
+    
+    // send the HTTP request to the token endpoint with the username and password credentials 
+    // in the request body
+    String reqBody = 'grant_type=password&username=' + uName + '&password=' + pWord; 
+	String tokenEndpoint = endpoint + '/token';
+    
+    HttpRequest req = new HttpRequest();
+    req.setMethod('POST');
+    req.setEndpoint(tokenEndpoint);
+    req.setBody(reqBody);
+    req.setHeader('Content-Type', 'application/x-www-form-urlencoded');
+    req.setHeader('Content-Length', '\'reqBody.length()\'');
+    
+    // Parse the response into JSON
+    HttpResponse res = new Http().send(req);
+    valence.JSONParse root = new valence.JSONParse(res.getBody());
+    
+    // assign the auth token to authToken
+    return root.get('access_token').getStringValue();
+}
 
-csvAttachment.setFileName(csvName);
-csvAttachment.setBody(csvBlob);
-Messaging.SingleEmailMessage email = new Messaging.SingleEmailMessage();
+// create .csv headers and data from the fields/keys
+private String createCsvString(List<String> fields, List<valence.JSONParse> data) {
 
-email.setSubject(subject);
-email.setToAddresses(toAddresses);
-email.setPlainTextBody('');
-email.setFileAttachments(new Messaging.EmailFileAttachment[] { csvAttachment } );
-Messaging.SendEmailResult[] result = Messaging.sendEmail(new Messaging.SingleEmailMessage[] {email});
+	String header;
+    String recordString = '';
+    String dataString = '';
+
+    for (valence.JSONParse school : data) {
+        header = '';
+        
+        // to avoid a 2nd for loop just to get the field headers, I set the headers during each loop through the data values
+        // I set header = '' at the start of each school loop so the headers will only be retained in the last run of the loop
+        // probably a better way to do this but I think performance impacts are negligible 
+        
+        for (String field : fields) {
+    		header = header + field + ',';
+            recordString = recordString + school.get(field).getStringValue() + ',';
+        }
+        dataString = dataString + recordString + '\n';
+        recordString = '';
+    }
+
+	dataString = header + '\n' + dataString;
+    
+    return dataString;
+}
+
+// send the .csv file in an email attachment
+private void sendCsvEmail(String csvName, String csvData, String emailSubject, List<String> toAddresses) {
+    
+    // create a blob from the csv data string
+    Blob csvBlob = Blob.valueOf(csvData);
+    
+    // create attachment
+    Messaging.EmailFileAttachment csvAttachment = new Messaging.EmailFileAttachment();
+    csvAttachment.setFileName(csvName);
+    csvAttachment.setBody(csvBlob);
+    
+    Messaging.SingleEmailMessage email = new Messaging.SingleEmailMessage();
+    email.setSubject(emailSubject);
+    email.setToAddresses(toAddresses);
+    email.setPlainTextBody('');
+    email.setFileAttachments(new Messaging.EmailFileAttachment[] { csvAttachment } );
+    Messaging.SendEmailResult[] result = Messaging.sendEmail(new Messaging.SingleEmailMessage[] {email});
+}
+
 
